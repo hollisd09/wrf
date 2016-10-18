d48ce7052dd5bbffac7c6ca8d791b3ff6fb44609
hollisd09 <hollisd09@gmail.com>
2016-10-17 23:24:04 -0500

    Added the logo, working on the scaling nav bar. Almost working, just needs some tweaking style wise.

--- script.js
+++ script.js
@@ -35,3 +35,21 @@
     $('.contact-wrf').removeClass('contact-wrf--show');
     $('.contact-wrf').addClass('contact-wrf--hidden');
   });
+
+/* NAVBAR SHRINK */
+
+function init() {
+  window.addEventListener('scroll', function(e) {
+    var distanceY = window.pageYOffset || document.documentElement.scrollTop,
+      shrinkOn = 300,
+      header = document.querySelector('header');
+    if (distanceY > shrinkOn) {
+      classie.add(header, 'smaller');
+    } else {
+      if (classie.has(header, 'smaller')) {
+        classie.remove(header, 'smaller');
+      }
+    }
+  });
+}
+window.onload = init();
