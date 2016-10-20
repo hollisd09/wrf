1ed9573b86eb075e9ede1f21aa16ebdb67dd6c48
hollisd09 <hollisd09@gmail.com>
2016-10-19 22:57:09 -0500

    Using an OWL slider. We shall see how it works. Still working on getting the background image to show up, but the slides are 'working'

--- script.js
+++ script.js
@@ -53,3 +53,19 @@ function init() {
   });
 }
 window.onload = init();
+
+
+/* CAROUSEL */
+
+$(document).ready(function() {
+
+  $("#owl-example").owlCarousel({
+    navigation : true,
+      slideSpeed : 300,
+      paginationSpeed : 400,
+      singleItem: true,
+      pagination: false,
+      rewindSpeed: 500
+  });
+
+});
